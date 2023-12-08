66cbd4144ea7f4bbe7e31aca1f3d6a5a6f6758a9
Jongwook-Seong <ypwa121@gmail.com>
2023-12-08 15:49:36 +0900

    remove conrtollers and views to set mvc

--- src/main/java/dokcerdevlink/profileservice/controller/Controller.java
+++
@@ -1,8 +0,0 @@
-package dokcerdevlink.profileservice.controller;
-
-import java.util.Map;
-
-public interface Controller {
-
-    String process(Map<String, String> paramMap, Map<String, Object> model);
-}
---
+++ src/main/java/dokcerdevlink/profileservice/controller/ProfileController.java
@@ -0,0 +1,7 @@
+package dokcerdevlink.profileservice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProfileController {
+}
